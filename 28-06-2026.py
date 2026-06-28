63541aabfe14c3c797ab9c0cef48193332d02971
adelian14 <aliadel922@gmail.com>
2026-06-28 21:15:10 +0300

    solved https://codeforces.com/contest/2156/problem/B

---
+++ 28-06-2026.py
@@ -0,0 +1,64 @@
+import os
+
+class TestcaseHelper:
+    def __init__(self):
+        self.local_env = True
+        self.input_lines = None
+        self.current_index = -1
+        testcases_path = '_codeforces_testcases.txt'
+        if os.path.exists(testcases_path):
+            with open(testcases_path,"r") as f:
+                s = f.read()
+            self.input_lines = s.split('\n')
+        else:
+            self.local_env = False
+    
+    def read_line(self):
+        if self.local_env:
+            self.current_index+=1
+            return self.input_lines[self.current_index]
+        return input()
+    
+    def read_int(self):
+        return int(self.read_line())
+    
+    def read_float(self):
+        return float(self.read_line())
+    
+    def read_ints(self):
+        line = self.read_line()
+        return [int(x) for x in line.split()]
+    
+    def read_floats(self):
+        line = self.read_line()
+        return [float(x) for x in line.split()]
+    
+    def read_strs(self, sep = ' '):
+        line = self.read_line()
+        return [x for x in line.split(sep=sep)]
+    
+    def print_arr(self, arr):
+        print(' '.join([str(x) for x in arr]))
+                
+helper = TestcaseHelper()
+
+t = helper.read_int()
+for _ in range(t):
+    n,q = helper.read_ints()
+    s = helper.read_line()
+    a = helper.read_ints()
+    if 'B' not in s:
+        for x in a:
+            print(x)
+        continue
+    for x in a:
+        i = 0
+        ans = 0
+        while x:
+            if s[i]=='A':
+                x-=1
+            else:
+                x=x//2
+            i = (i+1)%n
+            ans+=1
+        print(ans)
