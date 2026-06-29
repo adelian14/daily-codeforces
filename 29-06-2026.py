24b8b2301d777065e1af5203c9c6a3f955c6c40b
adelian14 <aliadel922@gmail.com>
2026-06-29 17:51:47 +0300

    solved https://codeforces.com/contest/2167/problem/D

---
+++ 29-06-2026.py
@@ -0,0 +1,59 @@
+import os
+import math
+
+class TestcaseHelper:
+    def __init__(self):
+        self.local_env = True
+        self.input_lines = None
+        self.current_index = -1
+        testcases_path = '_codeforces_testcases.txt'
+        if os.path.exists(testcases_path):
+            with open(testcases_path,"r") as f:
+                s = f.read()
+            self.input_lines = s.split('\n')
+        else:
+            self.local_env = False
+    
+    def read_line(self):
+        if self.local_env:
+            self.current_index+=1
+            return self.input_lines[self.current_index]
+        return input()
+    
+    def read_int(self):
+        return int(self.read_line())
+    
+    def read_float(self):
+        return float(self.read_line())
+    
+    def read_ints(self):
+        line = self.read_line()
+        return [int(x) for x in line.split()]
+    
+    def read_floats(self):
+        line = self.read_line()
+        return [float(x) for x in line.split()]
+    
+    def read_strs(self, sep = ' '):
+        line = self.read_line()
+        return [x for x in line.split(sep=sep)]
+    
+    def print_arr(self, arr):
+        print(' '.join([str(x) for x in arr]))
+                
+helper = TestcaseHelper()
+
+t = helper.read_int()
+for _ in range(t):
+    n = helper.read_int()
+    a = helper.read_ints()
+    ans = 10**18
+    for x in a:
+        k = 2
+        while k < ans:
+            if math.gcd(k,x)==1:
+                ans = min(ans,k)
+                break
+            k+=1
+            
+    print(ans)
