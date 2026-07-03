617fae41d5511527e9672f62dd644c4dedace14a
Ali Adel <aliadel@Alis-Mac-mini.local>
2026-07-03 22:39:16 +0300

    solved https://codeforces.com/contest/2123/problem/C

---
+++ 03-07-2026.py
@@ -0,0 +1,56 @@
+import os
+
+class TestcaseHelper:
+    def __init__(self):
+        self.local_env = True
+        self.input_lines = None
+        self.current_index = -1
+        testcases_path = '_codeforces_testcases.txt'
+        if os.path.exists(testcases_path):
+            with open(testcases_path,"r") as f:
+                s = f.read()
+            self.input_lines = s.split('\n')
+        else:
+            self.local_env = False
+    
+    def read_line(self):
+        if self.local_env:
+            self.current_index+=1
+            return self.input_lines[self.current_index]
+        return input()
+    
+    def read_int(self):
+        return int(self.read_line())
+    
+    def read_float(self):
+        return float(self.read_line())
+    
+    def read_ints(self):
+        line = self.read_line()
+        return [int(x) for x in line.split()]
+    
+    def read_floats(self):
+        line = self.read_line()
+        return [float(x) for x in line.split()]
+    
+    def read_strs(self, sep = ' '):
+        line = self.read_line()
+        return [x for x in line.split(sep=sep)]
+    
+    def print_arr(self, arr):
+        print(' '.join([str(x) for x in arr]))
+                
+helper = TestcaseHelper()
+
+t = helper.read_int()
+for _ in range(t):
+    n = helper.read_int()
+    a = helper.read_ints()
+    suf_max = [x for x in a]
+    pre_min = [x for x in a]
+    for i in range(1,n):
+        pre_min[i] = min(a[i],pre_min[i-1])
+    for i in range(n-2,-1,-1):
+        suf_max[i] = max(a[i],suf_max[i+1])
+    ans = ['1' if x==mn or x==mx else '0' for x,mn,mx in zip(a,pre_min,suf_max)]
+    print(''.join(ans))
