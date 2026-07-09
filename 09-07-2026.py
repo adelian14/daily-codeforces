37aab84a8744ed6234be114ee4d10f76386916fa
adelian14 <aliadel922@gmail.com>
2026-07-09 18:52:43 +0300

    solved https://codeforces.com/contest/2060/problem/B

---
+++ 09-07-2026.py
@@ -0,0 +1,64 @@
+import os
+
+class TestcaseHelper:
+    def __init__(self):
+        self.local_env = True
+        self.input_lines = None
+        self.current_index = -1
+        testcases_path = '_codeforces_testcases.txt'
+        if os.path.exists(testcases_path):
+            with open(testcases_path,"r") as f:
+                s = f.read()
+            self.input_lines = s.split('\n')
+        else:
+            self.local_env = False
+    
+    def read_line(self):
+        if self.local_env:
+            self.current_index+=1
+            return self.input_lines[self.current_index]
+        return input()
+    
+    def read_int(self):
+        return int(self.read_line())
+    
+    def read_float(self):
+        return float(self.read_line())
+    
+    def read_ints(self):
+        line = self.read_line()
+        return [int(x) for x in line.split()]
+    
+    def read_floats(self):
+        line = self.read_line()
+        return [float(x) for x in line.split()]
+    
+    def read_strs(self, sep = ' '):
+        line = self.read_line()
+        return [x for x in line.split(sep=sep)]
+    
+    def print_arr(self, arr):
+        print(' '.join([str(x) for x in arr]))
+                
+helper = TestcaseHelper()
+
+t = helper.read_int()
+for _ in range(t):
+    n,m = helper.read_ints()
+    p = [0 for _ in range(n)]
+    can = 1
+    for i in range(n):
+        a = helper.read_ints()
+        if not can:
+            continue
+        a.sort()
+        for j in range(1,m):
+            if a[j]-a[j-1]!=n:
+                can = 0
+                break
+        if can:
+            p[a[0]]=i+1
+    if can:
+        helper.print_arr(p)
+    else:
+        print(-1)
