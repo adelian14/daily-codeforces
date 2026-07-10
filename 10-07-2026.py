d6774b00a136a55339ab3c17178cce260837bea3
Ali Adel <aliadel922@gmail.com>
2026-07-10 22:11:27 +0300

    solved https://codeforces.com/contest/2132/problem/C1

---
+++ 10-07-2026.py
@@ -0,0 +1,53 @@
+import os
+
+class TestcaseHelper:
+    def __init__(self):
+        self.local_env = True
+        self.input_lines = None
+        self.current_index = -1
+        testcases_path = '_codeforces_testcases.txt'
+        if os.path.exists(testcases_path):
+            with open(testcases_path,"r") as f:
+                s = f.read()
+            self.input_lines = s.split('\n')
+        else:
+            self.local_env = False
+    
+    def read_line(self):
+        if self.local_env:
+            self.current_index+=1
+            return self.input_lines[self.current_index]
+        return input()
+    
+    def read_int(self):
+        return int(self.read_line())
+    
+    def read_float(self):
+        return float(self.read_line())
+    
+    def read_ints(self):
+        line = self.read_line()
+        return [int(x) for x in line.split()]
+    
+    def read_floats(self):
+        line = self.read_line()
+        return [float(x) for x in line.split()]
+    
+    def read_strs(self, sep = ' '):
+        line = self.read_line()
+        return [x for x in line.split(sep=sep)]
+    
+    def print_arr(self, arr):
+        print(' '.join([str(x) for x in arr]))
+                
+helper = TestcaseHelper()
+
+t = helper.read_int()
+for _ in range(t):
+    n = helper.read_int()
+    ans = 0
+    for x in range(21,-1,-1):
+        cnt = n // (3**x)
+        ans += cnt * (3**(x+1) + int(x*(3**(x-1))))
+        n-=cnt*(3**x)
+    print(ans)
