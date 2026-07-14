0cdd9c77f7dbe999e015039d7c7ac9b4de2f3317
Ali Adel <aliadel922@gmail.com>
2026-07-14 21:54:05 +0300

    solved https://codeforces.com/contest/1203/problem/A

---
+++ 14-07-2026.py
@@ -0,0 +1,71 @@
+import os
+
+class TestcaseHelper:
+    def __init__(self):
+        self.local_env = True
+        self.input_lines = None
+        self.current_index = -1
+        testcases_path = '_codeforces_testcases.txt'
+        if os.path.exists(testcases_path):
+            with open(testcases_path,"r") as f:
+                s = f.read()
+            self.input_lines = s.split('\n')
+        else:
+            self.local_env = False
+    
+    def read_line(self):
+        if self.local_env:
+            self.current_index+=1
+            return self.input_lines[self.current_index]
+        return input()
+    
+    def read_int(self):
+        return int(self.read_line())
+    
+    def read_float(self):
+        return float(self.read_line())
+    
+    def read_ints(self):
+        line = self.read_line()
+        return [int(x) for x in line.split()]
+    
+    def read_floats(self):
+        line = self.read_line()
+        return [float(x) for x in line.split()]
+    
+    def read_strs(self, sep = ' '):
+        line = self.read_line()
+        return [x for x in line.split(sep=sep)]
+    
+    def print_arr(self, arr):
+        print(' '.join([str(x) for x in arr]))
+                
+helper = TestcaseHelper()
+
+t = helper.read_int()
+for _ in range(t):
+    n = helper.read_int()
+    a = helper.read_ints()
+    idx = -1
+    for i in range(n):
+        if a[i]==1:
+            idx=i
+            break
+    f=[]
+    r=[]
+    for i in range(n):
+        f.append(a[(i+idx)%n])
+        r.append(a[(idx-i+n)%n])
+    ans = 2
+    for i,x in enumerate(f):
+        if i+1!=x:
+            ans-=1
+            break
+    for i,x in enumerate(r):
+        if i+1!=x:
+            ans-=1
+            break
+    if ans:
+        print("YES")
+    else:
+        print("NO")
