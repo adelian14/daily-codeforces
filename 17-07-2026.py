ccd119b6a4d69cceeb7dace731e9723e4f2623de
Ali Adel <aliadel922@gmail.com>
2026-07-17 21:39:34 +0300

    solved https://codeforces.com/contest/2241/problem/D

---
+++ 17-07-2026.py
@@ -0,0 +1,64 @@
+import os
+
+class TestcaseHelper:
+    def __init__(self):
+        self.local_env = True
+        self.input_lines = None
+        self.current_index = -1
+        testcases_path = '_codeforces_testcases.txt'
+        if os.path.exists(testcases_path):
+            with open(testcases_path,"r") as f:
+                s = f.read()
+            self.input_lines = s.split('\n')
+        else:
+            self.local_env = False
+    
+    def read_line(self):
+        if self.local_env:
+            self.current_index+=1
+            return self.input_lines[self.current_index]
+        return input()
+    
+    def read_int(self):
+        return int(self.read_line())
+    
+    def read_float(self):
+        return float(self.read_line())
+    
+    def read_ints(self):
+        line = self.read_line()
+        return [int(x) for x in line.split()]
+    
+    def read_floats(self):
+        line = self.read_line()
+        return [float(x) for x in line.split()]
+    
+    def read_strs(self, sep = ' '):
+        line = self.read_line()
+        return [x for x in line.split(sep=sep)]
+    
+    def print_arr(self, arr):
+        print(' '.join([str(x) for x in arr]))
+                
+helper = TestcaseHelper()
+
+t = helper.read_int()
+for _ in range(t):
+    n = helper.read_int()
+    a = helper.read_ints()
+    b = helper.read_ints()
+    for i in range(n-1,0,-1):
+        if a[i] < b[i]:
+            a[i]=b[i]
+        else:
+            d=a[i]-b[i]
+            a[i]=b[i]
+            a[i-1]+=d
+    if a[0] < b[0]:
+        a[0]=b[0]
+    ans = "YES"
+    for x,y in zip(a,b):
+        if x!=y:
+            ans = "NO"
+            break
+    print(ans)
