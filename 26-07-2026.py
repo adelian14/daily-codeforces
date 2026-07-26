cfc9ebb0e136afb3a35b413ca9ed132e194e5836
adelian14 <aliadel922@gmail.com>
2026-07-26 18:59:51 +0300

    solved https://codeforces.com/contest/2169/problem/A

---
+++ 26-07-2026.py
@@ -0,0 +1,63 @@
+import os
+
+class TestcaseHelper:
+    def __init__(self):
+        self.local_env = True
+        self.input_lines = None
+        self.current_index = -1
+        testcases_path = '_codeforces_testcases.txt'
+        if os.path.exists(testcases_path):
+            with open(testcases_path,"r") as f:
+                s = f.read()
+            self.input_lines = s.split('\n')
+        else:
+            self.local_env = False
+    
+    def read_line(self):
+        if self.local_env:
+            self.current_index+=1
+            return self.input_lines[self.current_index]
+        return input()
+    
+    def read_int(self):
+        return int(self.read_line())
+    
+    def read_float(self):
+        return float(self.read_line())
+    
+    def read_ints(self):
+        line = self.read_line()
+        return [int(x) for x in line.split()]
+    
+    def read_floats(self):
+        line = self.read_line()
+        return [float(x) for x in line.split()]
+    
+    def read_strs(self, sep = ' '):
+        line = self.read_line()
+        return [x for x in line.split(sep=sep)]
+    
+    def print_arr(self, arr):
+        print(' '.join([str(x) for x in arr]))
+                
+helper = TestcaseHelper()
+
+t = helper.read_int()
+for _ in range(t):
+    n,a = helper.read_ints()
+    nums = helper.read_ints()
+    ans = [0,0]
+    for i in range(2):
+        if i:
+            b = a-1
+        else:
+            b = a+1
+        for x in nums:
+            ax = abs(x-a)
+            bx = abs(b-x)
+            if bx > ax:
+                ans[i]+=1
+    if ans[0] > ans[1]:
+        print(a-1)
+    else:
+        print(a+1)
