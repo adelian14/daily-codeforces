053da2770505c8fc8ca37b0fc342809ad497f1a4
adelian14 <aliadel922@gmail.com>
2026-08-18 15:20:29 +0300

    solved https://codeforces.com/contest/2244/problem/D

---
+++ 18-08-2026.py
@@ -0,0 +1,60 @@
+import os
+
+class TestcaseHelper:
+    def __init__(self):
+        self.local_env = True
+        self.input_lines = None
+        self.current_index = -1
+        testcases_path = '_codeforces_testcases.txt'
+        if os.path.exists(testcases_path):
+            with open(testcases_path,"r") as f:
+                s = f.read()
+            self.input_lines = s.split('\n')
+        else:
+            self.local_env = False
+    
+    def read_line(self):
+        if self.local_env:
+            self.current_index+=1
+            return self.input_lines[self.current_index]
+        return input()
+    
+    def read_int(self):
+        return int(self.read_line())
+    
+    def read_float(self):
+        return float(self.read_line())
+    
+    def read_ints(self):
+        line = self.read_line()
+        return [int(x) for x in line.split()]
+    
+    def read_floats(self):
+        line = self.read_line()
+        return [float(x) for x in line.split()]
+    
+    def read_strs(self, sep = ' '):
+        line = self.read_line()
+        return [x for x in line.split(sep=sep)]
+    
+    def print_arr(self, arr):
+        print(' '.join([str(x) for x in arr]))
+                
+helper = TestcaseHelper()
+
+t = helper.read_int()
+for _ in range(t):
+    n,m = helper.read_ints()
+    a = helper.read_ints()
+    b = helper.read_ints()
+    p = [0]+[x for x in a]
+    for i in range(1,n+1):
+        p[i]+=p[i-1]
+    ans = 0
+    b.sort()
+    pre = 0
+    for i in range(m):
+        ans += abs(p[b[i]]-pre)
+        pre = p[b[i]]
+    ans += p[n]-pre
+    print(ans)
