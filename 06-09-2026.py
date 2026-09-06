440f4e4438fcf3fcb486f575b183f69fab1ec147
Ali Adel <aliadel922@gmail.com>
2026-09-06 18:22:39 +0300

    solved https://codeforces.com/contest/2259/problem/C

---
+++ 06-09-2026.py
@@ -0,0 +1,87 @@
+import os
+
+class TestcaseHelper:
+    def __init__(self):
+        self.local_env = True
+        self.input_lines = None
+        self.current_index = -1
+        testcases_path = '_codeforces_testcases.txt'
+        if os.path.exists(testcases_path):
+            with open(testcases_path,"r") as f:
+                s = f.read()
+            self.input_lines = s.split('\n')
+        else:
+            self.local_env = False
+    
+    def read_line(self):
+        if self.local_env:
+            self.current_index+=1
+            return self.input_lines[self.current_index]
+        return input()
+    
+    def read_int(self):
+        return int(self.read_line())
+    
+    def read_float(self):
+        return float(self.read_line())
+    
+    def read_ints(self):
+        line = self.read_line()
+        return [int(x) for x in line.split()]
+    
+    def read_floats(self):
+        line = self.read_line()
+        return [float(x) for x in line.split()]
+    
+    def read_strs(self, sep = ' '):
+        line = self.read_line()
+        return [x for x in line.split(sep=sep)]
+    
+    def print_arr(self, arr):
+        print(' '.join([str(x) for x in arr]))
+                
+helper = TestcaseHelper()
+
+t = helper.read_int()
+for _ in range(t):
+    n = helper.read_int()
+    a = helper.read_ints()
+    first = -1
+    last = -1
+    for i in range(n):
+        if a[i]==1:
+            first = i
+            break
+    for i in range(n-1,-1,-1):
+        if a[i]==1:
+            last = i
+            break
+    if first==-1:
+        for i in range(n):
+            if a[i]==-1:
+                a[i]=1
+                break
+        for i in range(n-1,-1,-1):
+            if a[i]==-1:
+                a[i]=1
+                break
+        for i in range(n):
+            if a[i]==-1:
+                a[i]=0
+    else:
+        for i in range(n):
+            if a[i]!=-1:
+                continue
+            if i < first:
+                a[i] = 1
+                break
+        for i in range(n-1,-1,-1):
+            if a[i]!=-1:
+                continue
+            if i > last:
+                a[i]=1
+                break
+        for i in range(n):
+            if a[i]==-1:
+                a[i]=0
+    helper.print_arr(a)
