16949e492c42b654a2e692dfcafb8ecd069202e0
Ali Adel <aliadel922@gmail.com>
2026-09-25 15:44:16 +0300

    solved https://codeforces.com/contest/1221/problem/B

---
+++ 25-09-2026.py
@@ -0,0 +1,79 @@
+import os
+
+class TestcaseHelper:
+    def __init__(self):
+        self.local_env = True
+        self.input_lines = None
+        self.current_index = -1
+        testcases_path = '_codeforces_testcases.txt'
+        if os.path.exists(testcases_path):
+            with open(testcases_path,"r") as f:
+                s = f.read()
+            self.input_lines = s.split('\n')
+        else:
+            self.local_env = False
+    
+    def read_line(self):
+        if self.local_env:
+            self.current_index+=1
+            return self.input_lines[self.current_index]
+        return input()
+    
+    def read_int(self):
+        return int(self.read_line())
+    
+    def read_float(self):
+        return float(self.read_line())
+    
+    def read_ints(self):
+        line = self.read_line()
+        return [int(x) for x in line.split()]
+    
+    def read_floats(self):
+        line = self.read_line()
+        return [float(x) for x in line.split()]
+    
+    def read_strs(self, sep = ' '):
+        line = self.read_line()
+        return [x for x in line.split(sep=sep)]
+    
+    def print_arr(self, arr):
+        print(' '.join([str(x) for x in arr]))
+                
+helper = TestcaseHelper()
+
+t = 1 # helper.read_int()
+for _ in range(t):
+    n = helper.read_int()
+    ans = [[0 for _ in range(n)] for _ in range(n)]
+    q = [[0,0,'W']]
+    idx = 0
+    while idx < len(q):
+        x,y,c = q[idx]
+        idx+=1
+        if ans[x][y]:
+            continue
+        ans[x][y] = c
+        p = [
+            [x-2,y-1],
+            [x-2,y+1],
+            [x+2,y-1],
+            [x+2,y+1],
+            [x-1,y-2],
+            [x-1,y+2],
+            [x+1,y-2],
+            [x+1,y+2],
+            ]
+        k = 'W'
+        if c=='W':
+            k= 'B'
+        for i,j in p:
+            if min(i,j) < 0 or max(i,j) >= n:
+                continue
+            q.append([i,j,k])
+    for i in range(n):
+        for j in range(n):
+            if not ans[i][j]:
+                ans[i][j] = 'W'
+    for r in ans:
+        print(''.join(r))
